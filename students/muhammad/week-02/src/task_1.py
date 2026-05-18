901e8183c118497d871924ca51770ae368c15308
Muhammad Bitar <mhmdhjmstf5@gmail.com>
2026-05-18 13:14:13 +0300

    Add account_num
    Add account number and add a method to validate account number, a method to create an account from dictionary.

--- students/muhammad/week-02/src/task_1.py
+++ students/muhammad/week-02/src/task_1.py
@@ -1,10 +1,29 @@
 class BankAccount():
 
-    def __init__(self, first_name: str, last_name: str, initial_balance: float = 0) -> None:
-        self.first_name = first_name
-        self.last_name = last_name
+    @classmethod
+    def from_dict(cls, dic: dict):
+        if 'account_num' not in dic:
+            raise ValueError("Dictionary must contain 'account_num")
+        if 'full_name' not in dic:
+            raise ValueError("Dictionary must contain 'full_name")
+        
+        if 'initial_balance' in dic:
+            return cls (dic['full_name'], dic['account_num'], dic['initial_balance']) 
+        
+        return cls (dic['full_name'], dic['account_num']) 
+        
+    @staticmethod
+    def is_valid_account_number(num)-> bool:
+            return isinstance(num, int)
+
+    def __init__(self, full_name: str, account_num: int, initial_balance: float = 0) -> None:
+        if not self.is_valid_account_number(account_num):
+            raise ValueError(f"Invalid account number format: {account_num}")
+        self.full_name = full_name
         self.__balance = initial_balance
+        self.account_num = account_num
         print(f"Account was created with ${initial_balance} initial balance.")
+
 
 
     def deposit(self, deposit_amount: float) -> None:
@@ -31,8 +50,25 @@ New balance: ${self.__balance}
 
 
 
-my_account: BankAccount = BankAccount("Muhammad", "Bitar", 20)
-print(my_account.get_balance())
-my_account.withdraw(10)
-my_account.deposit(20)
-my_account.withdraw(100)
+
+# bitar_acc = BankAccount("Muhammad Bitar", 123)
+# print(bitar_acc.full_name)
+# print(bitar_acc.account_num)
+# bitar_acc.deposit(200)
+# bitar_acc.deposit(400)
+# # bitar_acc.withdraw(601)
+# print(bitar_acc.get_balance())
+
+# amjad_acc = BankAccount.from_dict({
+#     "account_num": 124,
+#     "full_name": "Amjad Bakro"
+# })
+# # amjad_acc.withdraw(20)
+# amjad_acc.deposit(20)
+
+# amjad_acc = BankAccount.from_dict({
+#     "account_num": "123@",
+#     "full_name": "Amjad Bakro"
+# })
+# # amjad_acc.withdraw(20)
+# amjad_acc.deposit(20)
