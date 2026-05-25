e1f254d8624add20e4c2c4d74997f254ea8ee828
Muhammad Bitar <mhmdhjmstf5@gmail.com>
2026-05-25 20:10:28 +0300

    Uncomment task_05
    Fix code and uncomment the accidently three  commented lines of the file

--- students/muhammad/week-03/src/task_05.py
+++ students/muhammad/week-03/src/task_05.py
@@ -3,9 +3,9 @@ from itertools import islice
 def fibo():
     a = 0
     b = 1
-    # while a < 20:
-    #     yield a
-    #     a, b = b, b + a
+    while a < 20:
+        yield a
+        a, b = b, b + a
     yield a
     yield b
 
