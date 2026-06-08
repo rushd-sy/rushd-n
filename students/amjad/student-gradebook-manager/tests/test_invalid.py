2a439b5a4798d9f2f7b2884238c949cbb3e823ed
amjadbk <amjedbakro@gmail.com>
2026-06-08 18:07:23 +0300

    validation error now prints what happened exactly

--- students/amjad/student-gradebook-manager/tests/test_invalid.py
+++ students/amjad/student-gradebook-manager/tests/test_invalid.py
@@ -4,15 +4,15 @@ from services import add_student, delete_student, add_grade, update_student, imp
 from exceptions import EmailAlreadyUsedError, StudentNotFoundError
 
 def test_add_student_short_name(students):
-    with pytest.raises(ValidationError):
+    with pytest.raises(ValidationError, match="Name must be at least 3 characters long"):
         add_student(students, "b", "bakroexample.com", 25)
 
 def test_add_student_invalid_email(students):
-    with pytest.raises(ValidationError):
+    with pytest.raises(ValidationError, match="Invalid email format"):
         add_student(students, "bakro", "invalid-email", 25)
 
 def test_add_student_invalid_age(students):
-    with pytest.raises(ValidationError):
+    with pytest.raises(ValidationError, match="Age must be a positive integer"):
         add_student(students, "bakro", "bakro@example.com", -5)
 
 def test_add_student_duplicate_email(students):
@@ -28,7 +28,7 @@ def test_add_grade_student_not_found(students):
         add_grade(students, 5, "Math", 90)
 
 def test_add_grade_invalid_score(students):
-    with pytest.raises(ValidationError):
+    with pytest.raises(ValidationError, match="Score must be between 0 and 100"):
         add_grade(students, 1, "Math", -10)
 
 def test_update_student_not_found(students):
