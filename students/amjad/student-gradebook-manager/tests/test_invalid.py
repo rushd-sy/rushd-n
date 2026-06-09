c2c4db7b80414524584cb5d9a64a47fad6b9778a
amjadbk <amjedbakro@gmail.com>
2026-06-09 22:04:00 +0300

    fix some unmatched tests messages

--- students/amjad/student-gradebook-manager/tests/test_invalid.py
+++ students/amjad/student-gradebook-manager/tests/test_invalid.py
@@ -1,18 +1,18 @@
 from pydantic import ValidationError
 import pytest
 from services import add_student, delete_student, add_grade, update_student, import_students
-from exceptions import EmailAlreadyUsedError, StudentNotFoundError
+from exceptions import EmailAlreadyUsedError, StudentNotFoundError, InvalidGradeError
 
 def test_add_student_short_name(students):
-    with pytest.raises(ValidationError, match="Name must be at least 3 characters long"):
-        add_student(students, "b", "bakroexample.com", 25)
+    with pytest.raises(ValidationError, match="Name must be at least 2 characters long"):
+        add_student(students, "b", "bakro@example.com", 25)
 
 def test_add_student_invalid_email(students):
-    with pytest.raises(ValidationError, match="Invalid email format"):
+    with pytest.raises(ValidationError, match="value is not a valid email address"):
         add_student(students, "bakro", "invalid-email", 25)
 
 def test_add_student_invalid_age(students):
-    with pytest.raises(ValidationError, match="Age must be a positive integer"):
+    with pytest.raises(ValidationError, match="Age must be between 16 and 60"):
         add_student(students, "bakro", "bakro@example.com", -5)
 
 def test_add_student_duplicate_email(students):
@@ -28,7 +28,7 @@ def test_add_grade_student_not_found(students):
         add_grade(students, 5, "Math", 90)
 
 def test_add_grade_invalid_score(students):
-    with pytest.raises(ValidationError, match="Score must be between 0 and 100"):
+    with pytest.raises(InvalidGradeError, match="Score must be between 0 and 100"):
         add_grade(students, 1, "Math", -10)
 
 def test_update_student_not_found(students):
