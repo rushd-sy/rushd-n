5cf16273149e3c19cd2b2ca8499c2b7d30f52b19
Muhammad Bitar <mhmdhjmstf5@gmail.com>
2026-06-15 00:02:27 +0300

    Created Python script to fetch GitHub repos
    Used HTTPX with Python to fetch  GitHub repos and sorted them descendely based on the number of stars for each repo.

---
+++ students/muhammad/week-04/src/Exercises/task_01.py
@@ -0,0 +1,41 @@
+import httpx
+from pydantic import BaseModel
+
+class Repo(BaseModel):
+    name: str
+    stars: int
+    language: str | None
+    url: str
+
+
+
+def main():
+    url = "https://api.github.com/users/mohammedbabelly20/repos"
+    params = {
+        "sort" : "starts",
+        "direction" : "desc",
+        "per_page" : 10
+    }
+
+    response = httpx.get(url, params=params)
+    data = response.json()
+    repos: list[Repo] = []
+
+    for repo_data in data:
+        repo = Repo( 
+            name=repo_data['name'],
+            stars=int(repo_data['stargazers_count']),
+            language=repo_data['language'],
+            url=repo_data['html_url']
+        )
+        repos.append(repo)
+    
+    for ind, repo in enumerate(repos):
+        print(f"Repo's num: {ind}")
+        print(f"\tRepo's name: {repo.name}")
+        print(f"\tRepo's stars: {repo.stars}")
+        print(f"\tRepo's language: {repo.language}")
+        print(f"\tRepo's url: {repo.url}")
+
+if __name__ == '__main__':
+    main()
