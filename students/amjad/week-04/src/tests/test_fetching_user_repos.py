b9b46038aca1f9d779aad35be6a7fa65dbfa9bc8
amjadbk <amjedbakro@gmail.com>
2026-06-16 23:26:11 +0300

    the tests now uses mock

--- students/amjad/week-04/src/tests/test_fetching_user_repos.py
+++ students/amjad/week-04/src/tests/test_fetching_user_repos.py
@@ -1,8 +1,33 @@
+import httpx
 import pytest
-
 from fetching_user_repos import fetch_repositories
 
-def test_fetch_repositories_valid_user():
+def test_fetch_repositories_valid_user(respx_mock):
+
+    fetch_url = "https://api.github.com/search/repositories"
+
+    params = {
+        "q": f"user:octocat",
+        "sort": "stars",
+        "order": "desc",
+        "per_page": 10
+    }
+
+    respx_mock.get(fetch_url, params=params).mock(
+        return_value=httpx.Response(
+            200, 
+            json={
+                "items": [
+                    {
+                        "name": "Hello-World",
+                        "language": "Python",
+                        "stargazers_count": 42,
+                        "html_url": "https://github.com/octocat/Hello-World"
+                    }
+                ]
+            }
+            )
+    )
     repos = fetch_repositories("octocat")
     assert isinstance(repos, list)
     assert len(repos) > 0
@@ -12,6 +37,15 @@ def test_fetch_repositories_valid_user():
         assert hasattr(repo, 'stars')
         assert hasattr(repo, 'url')
 
-def test_fetch_repositories_invalid_user():
+def test_fetch_repositories_invalid_user(respx_mock):
+
+    params = {
+        "q": f"user:djfahlsieuhcvsaol",
+        "sort": "stars",
+        "order": "desc",
+        "per_page": 10
+    }
+
+    respx_mock.get("https://api.github.com/search/repositories", params=params).mock(return_value=httpx.Response(422))
     with pytest.raises(SystemExit, match="User 'djfahlsieuhcvsaol' not found"):
         fetch_repositories("djfahlsieuhcvsaol")
--- students/amjad/week-04/src/tests/test_fetching_using_async.py
+++ students/amjad/week-04/src/tests/test_fetching_using_async.py
@@ -1,9 +1,27 @@
-from fetching_using_async import fetch_data, fetch_data_sync
 import asyncio
 
-def test_fetch_data():
+from fetching_using_async import fetch_data, fetch_data_sync
+import httpx
+import pytest
+import time
+
+def sleep_sync(request):
+    time.sleep(1)
+    return httpx.Response(200, json={"status": "ok"})
+
+async def sleep_async(request):
+    await asyncio.sleep(1)
+    return httpx.Response(200, json={"status": "ok"})
+
+@pytest.mark.asyncio
+async def test_fetch_data(respx_mock):
     fetch_url = "https://api.github.com/repos/python/cpython"
     urls = [fetch_url] * 10
+    respx_mock.get(fetch_url).mock(side_effect=sleep_sync)
     sync_time = fetch_data_sync(urls)
-    async_time = asyncio.run(fetch_data(urls))
+
+    respx_mock.clear()
+
+    respx_mock.get(fetch_url).mock(side_effect=sleep_async)
+    async_time = await fetch_data(urls)
     assert async_time < sync_time
