1853a0d5e9f1e540f6dfe6b60fb61553ad58eac9
amjadbk <amjedbakro@gmail.com>
2026-07-28 19:05:05 +0300

    fixed get_current_user bug that lead to 403 branch to be a dead code

--- students/amjad/week-07/src/dependency.py
+++ students/amjad/week-07/src/dependency.py
@@ -8,9 +8,9 @@ async def get_pagination_params(
     return {"limit": limit, "offset": offset}
 
 async def get_current_user(
-        X_user_id: Annotated[int | None, Header()] = 1,
-):
-    return {"user_id": X_user_id}
+        X_user_id: Annotated[int | None, Header()] = None,
+) -> int | None:
+    return X_user_id
 
 CommonsDepForPagination = Annotated[dict, Depends(get_pagination_params)]
 CurrentUserDep = Annotated[int, Depends(get_current_user)]
