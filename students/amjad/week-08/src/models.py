82db500e91a9b91c19547af5d3d73be6ac9230f3
amjadbk <amjedbakro@gmail.com>
2026-08-09 23:25:03 +0300

    Added more models validations for tests

--- students/amjad/week-08/src/models.py
+++ students/amjad/week-08/src/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import TypeVar, Generic
 
 T = TypeVar('T')
@@ -11,10 +11,10 @@ class Page(BaseModel, Generic[T]):
 
 
 class BookCreate(BaseModel):
-    title: str
-    author: str
-    genre: str
-    year: int
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1)
+    genre: str = Field(min_length=1)
+    year: int = Field(gt=0)
 
 class BookOut(BaseModel):
     book_id: int
@@ -39,9 +39,9 @@ class Loan(BaseModel):
     return_date: str
 
 class LoanCreate(BaseModel):
-    book_id: int
-    user_id: int
-    return_date: str
+    book_id: int = Field(gt=0)
+    user_id: int = Field(gt=0)
+    return_date: str = Field(min_length=1)
 
 class LoanOut(BaseModel):
     loan_id: int
@@ -62,5 +62,5 @@ class AuthorOut(BaseModel):
     birth_year: int
 
 class AuthorCreate(BaseModel):
-    name: str
-    birth_year: int
+    name: str = Field(min_length=1)
+    birth_year: int = Field(gt=0)
