fee831ee3106dbc53ff859de5b025b9daeb522ed
amjadbk <amjedbakro@gmail.com>
2026-08-21 22:19:16 +0300

    password now protected with hashing

--- students/amjad/week-09/src/services/auth_service.py
+++ students/amjad/week-09/src/services/auth_service.py
@@ -2,14 +2,25 @@ from fastapi import HTTPException
 from models import LoginData, UserOut, UserCreate, User
 from datetime import datetime
 from storage import load_users, save_users
+from pwdlib import PasswordHash
+
 
 
 class UserService:
+    def __init__(self):
+        self.password_hash = PasswordHash.recommended()
+
+    def get_password_hash(self, password: str) -> str:
+        return self.password_hash.hash(password)
+
+    def verify_password(self, password: str, hashed_password: str) -> bool:
+        return self.password_hash.verify(password, hashed_password)
+
     async def login(self, login_data: LoginData) -> UserOut:
         email, password = login_data.email, login_data.password
         users = await load_users()
         for user in users:
-            if user.email == email and user.password == password:
+            if user.email == email and self.verify_password(password, user.password):
                 return UserOut(**user.model_dump())
         raise HTTPException(status_code=401, detail="unauthorized")
 
@@ -21,7 +32,7 @@ class UserService:
             name=user_create.name,
             email=user_create.email,
             created_at=datetime.now().isoformat(),
-            password=user_create.password
+            password=self.get_password_hash(user_create.password)
         )
         users.append(new_user)
         await save_users(users)
